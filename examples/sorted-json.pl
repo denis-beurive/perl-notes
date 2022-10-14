2027d46747fbcdf7d8878f3f48b7796c64505165
Denis Beurivé <dbeurive@stamus-networks.com>
2022-10-14 22:29:56 +0200

    add more interesting example

--- examples/sorted-json.pl
+++ examples/sorted-json.pl
@@ -12,7 +12,13 @@ sub json_beautify {
 my $hash = {
 	ghi => 3,
 	def => 2,
-	abc => 1
+	abc => 1,
+	0 => {
+		d => 1,
+		c => 2,
+		b => 3,
+		a => 4
+	}
 };
 
 printf("Sorted hash is:\n\n%s\n\n", json_beautify($hash));
@@ -20,8 +26,13 @@ printf("Sorted hash is:\n\n%s\n\n", json_beautify($hash));
 # Result:
 #
 # {
-#   "abc" : 1,
-#   "def" : 2,
-#   "ghi" : 3
+#    "0" : {
+#       "a" : 4,
+#       "b" : 3,
+#       "c" : 2,
+#       "d" : 1
+#    },
+#    "abc" : 1,
+#    "def" : 2,
+#    "ghi" : 3
 # }
-
